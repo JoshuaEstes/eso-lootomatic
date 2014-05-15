bc4765cd59962608ef9137d4e71c4247727de53e
Joshua Estes <Joshua@Estes.in>
2014-05-14 21:03:17 -0500

    Still working on a few things

--- Lootomatic.lua
+++ Lootomatic.lua
@@ -2,11 +2,37 @@
 --
 -- Lootomatic
 --
+-- Manages loot by allowing you to set filters on what you
+-- want to keep and what you want to mark as junk to sell
+-- at your friendly vendor.
+--
 --]]
-
 local lootomatic = {}
 lootomatic.name = 'Lootomatic'
 lootomatic.defaults = {}
+
+--[[
+-- eso Item class, helps to get information about an item
+--]]
+LootItem = {}
+LootItem.__index = LootItem
+function LootItem.New(itemName)
+    local self = setmetatable({}, LootItem)
+    self.name,  self.color,
+    self.n3,  self.id,
+    self.n5,  self.n6,
+    self.n7,  self.n8,
+    self.n9,  self.n10,
+    self.n11, self.n12,
+    self.n13, self.n14,
+    self.n15, self.n16,
+    self.n17, self.n18,
+    self.n19, self.n20,
+    self.n21, self.n22,
+    self.n23, self.n24 = ZO_LinkHandler_ParseLink(itemName)
+    self.icon, self.sellPrice, self.meetsUsageRequirement, self.equipType, self.itemStyle = GetItemLinkInfo(itemName)
+    return self
+end
 
 --[[
 -- @param integer eventCode
@@ -36,11 +62,14 @@ end
 -- @param boolean isSelf
 --]]
 function lootomatic.onLootReceived(eventCode, lootedBy, itemName, quantity, itemSound, lootType, isSelf)
-    if (not isSelf) then return end
     d('onLootReceived')
+    if (not isSelf) then return end
+    local i = LootItem.New(itemName)
+    d(i)
     --[[
+	local icon, sellPrice, meetsUsageRequirement, equipType, itemStyle = GetItemLinkInfo(itemName)
+    d('itemName: ' .. i.GetName())
     d('lootedBy: ' .. lootedBy)
-    d('itemName: ' .. itemName)
     d('quantity: ' .. quantity)
     d('itemSound: ' .. itemSound)
     d('lootType: ' .. lootType)
@@ -71,6 +100,18 @@ end
 
 --[[
 -- @param integer eventCode
+-- @param integer bagId
+-- @param integer slotId
+-- @param boolean isNewItem
+-- @param integer itemSoundCategory
+-- @param integer updateReason
+--]]
+function lootomatic.onInventorySingleSlotUpdate(eventCode, bagId, slotId, isNewItem, itemSoundCategory, updateReason)
+    d('onInventorySingleSlotUpdate')
+end
+
+--[[
+-- @param integer eventCode
 -- @param string  addOnName
 --]]
 function lootomatic.onAddOnLoaded(eventCode, addOnName)
@@ -88,6 +129,9 @@ function lootomatic.onAddOnLoaded(eventCode, addOnName)
     -- Vendor events
     EVENT_MANAGER:RegisterForEvent(lootomatic.name, EVENT_CLOSE_STORE, lootomatic.onCloseStore)
     EVENT_MANAGER:RegisterForEvent(lootomatic.name, EVENT_OPEN_STORE, lootomatic.onOpenStore)
+
+    -- Inventory Events
+    EVENT_MANAGER:RegisterForEvent(lootomatic.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, lootomatic.onInventorySingleSlotUpdate)
 end
 
 EVENT_MANAGER:RegisterForEvent(lootomatic.name, EVENT_ADD_ON_LOADED, lootomatic.onAddOnLoaded)
