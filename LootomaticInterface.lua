a4b543d638a855fe2e084786d06da346419e929f
Joshua Estes <Joshua@Estes.in>
2014-05-26 13:29:10 -0500

    Removed code so it won't conflict with other code

--- LootomaticInterface.lua
+++ LootomaticInterface.lua
@@ -1,24 +1,5 @@
 --[[
+--
 -- Lootomatic GUI Interface
+--
 --]]
-
-LootomaticInterface = {}
-LootomaticInterface.__index = LootomaticInterface
-local LAM = LibStub( 'LibAddonMenu-1.0' )
-
-function LootomaticInterface.New()
-    local self = setmetatable({}, LootomaticInterface)
-	self.panel = LAM:CreateControlPanel("LootomaticSettingsPanel", "Lootomatic")
-	LAM:AddHeader(self.panel, "Lootomatic_Header_Configuration", "Configuration")
-    LAM:AddCheckbox(self.panel, 'debug', 'Debug', 'Tooltip',
-        function() return true end,
-        function() self:Test() end)
-	LAM:AddHeader(self.panel, "Lootomatic_Header_Filters", "Filters")
-    return self
-end
-
-function LootomaticInterface:Test()
-    LAM:AddCheckbox(self.panel, 'another_Filter', 'Debug', 'Tooltip',
-        function() return true end,
-        function() d('touched') end)
-end
